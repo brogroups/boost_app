cba513d39e3c2cad348a0034317db80d19afa5e1
Takitok2011 <bekzodeoke@gmail.com>
2025-02-20 20:50:57 +0300

    m

--- src/controllers/static.controller.js
+++ src/controllers/static.controller.js
@@ -2,8 +2,7 @@ const OrderWithDeliveryModel = require("../models/orderWithDelivery.model")
 const SellingBreadModel = require("../models/sellingBread.model")
 const Debt2Model = require("../models/debt2.model")
 const { default: mongoose } = require("mongoose")
-const TypeOfBreadModel = require("../models/typOfbread.model")
-
+ 
 exports.getStatics = async (req, res) => {
     try {
         const use = req.use
@@ -23,26 +22,27 @@ exports.getStatics = async (req, res) => {
         }
         else {
             let orderWithDeliveries = await OrderWithDeliveryModel.find({}).populate("typeOfBreadIds sellerId")
-            orderWithDeliveries = orderWithDeliveries.map((item) => {
+            let prixod = orderWithDeliveries.map((item) => {
                 return { ...item._doc, price: item.typeOfBreadIds.reduce((a, b) => a + b.price, 0) * item.quantity }
             })
 
             let sellingBreads = await SellingBreadModel.find({}).populate("typeOfBreadIds deliveryId")
             sellingBreads = sellingBreads.map((item) => {
                 const price = item.typeOfBreadIds.reduce((a, b) => a + b.price, 0)
-                console.log(price);
-
                 return { ...item._doc, price: price * item.quantity }
             })
 
             let debt2s = await Debt2Model.find({}).populate("debtId sellerId")
+
+
+
             return res.status(200).json({
                 success: true,
                 message: "list of statics",
                 statics: {
-                    prixod: [...orderWithDeliveries, ...sellingBreads],
+                    prixod: [...prixod, ...sellingBreads],
                     rasxod: debt2s,
-                    panding: [],
+                    panding: orderWithDeliveries,
                 }
             })
         }
