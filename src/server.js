e4de3acb3ea12b417dcabe2906693c8f4435df32
azamjonbro <azamjondasturchi11@gmail.com>
2025-02-20 21:35:11 +0500

    edited cors with server test

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 app.use(cors({
-    origin: "*", 
+    origin: "http://localhost:8080", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
