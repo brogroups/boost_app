cac1c63b131ef68f05fed454e2e91101884e6281
azamjonbro <azamjondasturchi11@gmail.com>
2025-02-26 20:13:10 +0500

    added server.js config

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    // origin:"https://alphae.uz",
+    origin: ["http://localhost:8080","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
