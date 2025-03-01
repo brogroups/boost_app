1bd36dd66803e4a756f249c9f50daffc9745a54b
Bekzod <bekzodeoke@gmail.com>
2025-03-02 01:32:15 +0500

    commit 7-

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin:"http://localhost:8080",
-    origin: ["http://localhost:8080","http://localhost:5173","https://safo-non.netlify.app"],
+    origin: ["http://localhost:8080","http://localhost:5173","https://safo-non.netlify.app","https://alphae.uz"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
