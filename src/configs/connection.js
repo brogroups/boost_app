25cdb051752253aa8c9df0f7dcd70b73bf60c762
azamjonbro <azamjondasturchi11@gmail.com>
2025-03-03 01:30:09 +0500

    git push all cange

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,14 +1,17 @@
 const { connect } = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
 const ConnecToDb = async () => {
-    console.log("mongodb is loading");
-    try{
-        await connect(process.env.MONGO_URL)
-        console.log("mongodb is connected");
+    console.log("MongoDB is loading...");
+    try {
+        await connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB is connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
     }
-    catch(error){
-        console.error("mongodb is not connected error:",error)
-    }
-}
-module.exports = ConnecToDb
+};
+
+module.exports = ConnecToDb;
