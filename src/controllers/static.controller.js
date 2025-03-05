a06f7159e56893871b354a1ea6147c90721cd00e
Bekzod <bekzodeoke@gmail.com>
2025-03-05 23:38:11 +0500

    changed static

--- src/controllers/static.controller.js
+++ src/controllers/static.controller.js
@@ -4,9 +4,22 @@ const DeliveryDebtModel = require("../models/deliveryDebt.model")
 
 exports.getStatics = async (req, res) => {
     try {
-        const debt1s = await Debt1Model.find({}).populate("sellerId")
-        const debt2s = await Debt2Model.find({}).populate("sellerId")
-        const deliveryDebt = await DeliveryDebtModel.find({}).populate("deliveryId")
+        let debt1s = await Debt1Model.find({}).populate("sellerId", 'username')
+        let debt2s = await Debt2Model.find({}).populate("sellerId", 'username')
+        let deliveryDebt = await DeliveryDebtModel.find({}).populate("deliveryId", 'username')
+
+        debt1s = debt1s.map((item) => {
+            return { ...item._doc, role: "seller" }
+        })
+
+        debt2s = debt2s.map((item) => {
+            return { ...item._doc, role: "seller" }
+        })
+
+        deliveryDebt = deliveryDebt.map((item) => {
+            return { ...item._doc, role: "delivery" }
+        })
+
         return res.status(200).json({
             statics: [...debt1s, ...debt2s, ...deliveryDebt],
             managerStatics: {}
