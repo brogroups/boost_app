57d57f1a122c88d1da9f3fe71e8ff688cc60c90b
Bekzod <bekzodeoke@gmail.com>
2025-03-13 14:41:19 +0500

    project is good

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,19 +1,12 @@
 const { connect } = require("mongoose");
-const CustomEncryptor = require("custom-decryptor"); 
 require("dotenv").config();
 
-const decryptor = new CustomEncryptor(); 
 
-const encryptedMongoUrl = process.env.HASHED_MONGO_URL; 
-
-// 🔓 MongoDB URL ni decrypt qilish
-const mongoUrl = decryptor.decrypt(encryptedMongoUrl);
-console.log("Decrypted MongoDB URL:", mongoUrl);
 
 const ConnecToDb = async () => {
     console.log("MongoDB is loading...");
     try {
-        await connect(mongoUrl);
+        await connect(process.env.MONGO_URL);
         console.log("MongoDB is connected");
     } catch (error) {
         console.error("MongoDB connection error:", error);
--- src/models/orderWithDelivery.model.js
+++ src/models/orderWithDelivery.model.js
@@ -15,7 +15,7 @@ const OrderWithDeliverySchema = new Schema({
     description: { type: String, required: true },
     sellerId: { type: Schema.Types.ObjectId, ref: SellerModel, required: true },
     deliveryId: { type: Schema.Types.ObjectId, ref: DeliveryModel, required: true },
-    magazineId: { type: Schema.Types.ObjectId, ref: MagazineModel, required: true },
+    // magazineId: { type: Schema.Types.ObjectId, ref: MagazineModel, required: true },
     createdAt: { type: Date, default: new Date() },
     updateAt: { type: Date, default: new Date() }
 })
