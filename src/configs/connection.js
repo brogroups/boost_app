2c607a3b68735c47eea87f0c274d9acc771a1dbe
azamjonbro <azamjondasturchi11@gmail.com>
2025-03-13 22:54:41 +0500

    addd

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -1,17 +1,16 @@
 const { connect } = require("mongoose");
+const CustomEncryptor = require("custom-decryptor"); 
 require("dotenv").config();
 
+const decryptor = new CustomEncryptor(); 
 
-<<<<<<< HEAD
 const encryptedMongoUrl = process.env.HASHED_MONGO_URL; 
 const mongoUrl = decryptor.decrypt(encryptedMongoUrl);
-=======
->>>>>>> 78db24db7178d1e1be19a6971be8029b4c882f44
 
 const ConnecToDb = async () => {
     console.log("MongoDB is loading...");
     try {
-        await connect(process.env.MONGO_URL);
+        await connect(mongoUrl);
         console.log("MongoDB is connected");
     } catch (error) {
         console.error("MongoDB connection error:", error);
