978f5eed803ed086aaa2a73e28f46459967cc613
Bekzod <bekzodeoke@gmail.com>
2025-04-06 04:15:10 +0500

    changed code

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -1,9 +1,9 @@
 const Redis = require("ioredis")
 
 const redis = new Redis({
-    host: 'redis-13277.c246.us-east-1-4.ec2.redns.redis-cloud.com',
-    port: 13277,
-    password: "CA3cavbFkDGHY7qzLR6AFRDIxAQ6CueN",
+    host: 'redis-16925.c82.us-east-1-2.ec2.redns.redis-cloud.com',
+    port: 16925,
+    password: "SafAy9I49LDruGhPZGsAHBHqRNhkh3zX",
     // host: 'redis-14180.c267.us-east-1-4.ec2.redns.redis-cloud.com',
     // port: 14180,
     // password: "Q02P20co7KY6RL6QM19gcDMlXa5nwBj7",
